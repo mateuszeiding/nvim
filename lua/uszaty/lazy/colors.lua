3b8966503b18c9a8dd44f98cb346679a1bba8f65
MATEUSZ EIDING <eidingmateusz@gmail.com>
2024-12-18 17:42:48 +0100

    Correct colors

--- lua/uszaty/lazy/colors.lua
+++ lua/uszaty/lazy/colors.lua
@@ -2,23 +2,22 @@ function ColorMyPencils(color)
 	color = color or "rose-pine-moon"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg= "none", sp = "#C80A95" })
+    vim.api.nvim_set_hl(0, "StatusLine", { bg = "#D3D3D3", fg = "#000000" })
+    vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "ColorColumn", { fg = "#ADD8E6", underline = true })
+    vim.api.nvim_set_hl(0, "LineNr", { bg = "none", fg = "#F5F5F5" })
+    vim.api.nvim_set_hl(0, "LineNrAbove", { bg = "none", fg = "#ADD8E6" })
+    vim.api.nvim_set_hl(0, "LineNrBelow", { bg = "none", fg = "#ADD8E6" })
+    vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none", fg = "#F8F8FF" })
+    vim.api.nvim_set_hl(0, "VertSplit", { bg = "none", fg = "none" })
 end
 
 return {
 
     {
         "erikbackman/brightburn.vim",
-    },
-
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        opts = {},
-        config = function()
-            ColorMyPencils()
-        end
     },
     {
         "ellisonleao/gruvbox.nvim",
@@ -53,20 +52,13 @@ return {
     {
         "folke/tokyonight.nvim",
         config = function()
+            ColorMyPencils()
             require("tokyonight").setup({
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
                 transparent = true, -- Enable this to disable setting the background color
                 terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
                 styles = {
-                    -- Style to be applied to different syntax groups
-                    -- Value is any valid attr-list value for `:help nvim_set_hl`
                     comments = { italic = false },
                     keywords = { italic = false },
-                    -- Background styles. Can be "dark", "transparent" or "normal"
-                    sidebars = "dark", -- style for sidebars, see below
-                    floats = "dark", -- style for floating windows
                 },
             })
         end
